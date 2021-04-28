663ec2c35b506dc9237d51c9ea26e63931ee928c
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2021-04-29 01:29:44 +0300

    Mongo DB config added for rabbit mq to consume logs

---
+++ DatabaseAccess/MongoDB.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace DatabaseAccess
+{
+    public class LogRecord
+    {
+        public ObjectId Id { get; set; }
+        public string Log { get; set; }
+
+        public static implicit operator LogRecord(string log) => new LogRecord() { Log = log };
+    }
+
+    public class MongoRepo
+    {
+        private static IMongoClient _client = new MongoClient("mongodb://localhost:27017");
+        private static IMongoDatabase _logDatabase = _client.GetDatabase("NET-Template");
+        private static IMongoCollection<LogRecord> _collection = _logDatabase.GetCollection<LogRecord>("Logs");
+
+        public static void InsertLog(string log) => _collection.InsertOne(log);
+    }
+}
