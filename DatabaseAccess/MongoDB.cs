59c0a354020fc19828854ca9bbf6df808144b2a8
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2021-04-29 23:49:04 +0300

    log collection converted to bsondocument

--- DatabaseAccess/MongoDB.cs
+++ DatabaseAccess/MongoDB.cs
@@ -1,22 +1,15 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Driver;
+using System;
 
 namespace DatabaseAccess
 {
-    public class LogRecord
-    {
-        public ObjectId Id { get; set; }
-        public string Log { get; set; }
-
-        public static implicit operator LogRecord(string log) => new LogRecord() { Log = log };
-    }
-
     public class MongoRepo
     {
         private static IMongoClient _client = new MongoClient("mongodb://localhost:27017");
         private static IMongoDatabase _logDatabase = _client.GetDatabase("LOGDB");
-        private static IMongoCollection<LogRecord> _collection = _logDatabase.GetCollection<LogRecord>("Logs");
+        private static IMongoCollection<BsonDocument> _collection = _logDatabase.GetCollection<BsonDocument>("Logs");
 
-        public static void InsertLog(string log) => _collection.InsertOne(log);
+        public static void InsertLog(string log) => _collection.InsertOne(BsonDocument.Parse(log));
     }
 }
