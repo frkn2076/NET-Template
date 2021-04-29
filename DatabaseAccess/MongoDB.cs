e4830055c1becf0fda246443ae2b4a65936c31d9
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2021-04-29 19:47:29 +0300

    Db and collection renamed

--- DatabaseAccess/MongoDB.cs
+++ DatabaseAccess/MongoDB.cs
@@ -14,7 +14,7 @@ namespace DatabaseAccess
     public class MongoRepo
     {
         private static IMongoClient _client = new MongoClient("mongodb://localhost:27017");
-        private static IMongoDatabase _logDatabase = _client.GetDatabase("NET-Template");
+        private static IMongoDatabase _logDatabase = _client.GetDatabase("LOGDB");
         private static IMongoCollection<LogRecord> _collection = _logDatabase.GetCollection<LogRecord>("Logs");
 
         public static void InsertLog(string log) => _collection.InsertOne(log);
