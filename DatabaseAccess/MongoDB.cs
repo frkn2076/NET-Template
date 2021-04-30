b4baa5dec60bba153e0c1640b515d9be44c9fdc7
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2021-05-01 01:29:28 +0300

    Mongo db connection managed by env. variables

--- DatabaseAccess/MongoDB.cs
+++ DatabaseAccess/MongoDB.cs
@@ -6,7 +6,9 @@ namespace DatabaseAccess
 {
     public class MongoRepo
     {
-        private static IMongoClient _client = new MongoClient("mongodb://localhost:27017");
+        private static string _mongoUser = Environment.GetEnvironmentVariable("MongoUser");
+        private static string _mongoPassword = Environment.GetEnvironmentVariable("MongoPassword");
+        private static IMongoClient _client = new MongoClient($"mongodb://{_mongoUser}:{_mongoPassword}@localhost:27017");
         private static IMongoDatabase _logDatabase = _client.GetDatabase("LOGDB");
         private static IMongoCollection<BsonDocument> _collection = _logDatabase.GetCollection<BsonDocument>("Logs");
 
