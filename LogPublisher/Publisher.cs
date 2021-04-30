2d627a627c1861da6dbc23e046796bd396a37abd
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2021-05-01 01:37:26 +0300

    Publisher configs changed to feed by env. variables

--- LogPublisher/Publisher.cs
+++ LogPublisher/Publisher.cs
@@ -1,24 +1,27 @@
 ﻿using RabbitMQ.Client;
+using System;
 using System.Text;
 
 namespace LogPublisher
 {
     public class Publisher
     {
-        private static ConnectionFactory _factory = new ConnectionFactory() { HostName = "localhost" };
-        private static IConnection _connection = _factory.CreateConnection();
-        private static IModel _channel = QueueDeclare("logging");
-        private static IModel QueueDeclare(string queue)
+        private static readonly string _loggingQueue = Environment.GetEnvironmentVariable("LoggingQueue");
+        private static readonly string _logQueueHostName = Environment.GetEnvironmentVariable("LogQueueHostName");
+        private static readonly ConnectionFactory _factory = new ConnectionFactory() { HostName = _logQueueHostName };
+        private static readonly IConnection _connection = _factory.CreateConnection();
+        private static IModel _channel => DeclareQueue();
+        private static IModel DeclareQueue()
         {
             var channel = _connection.CreateModel();
-            channel.QueueDeclare(queue: queue, durable: false, exclusive: false, autoDelete: false, arguments: null);
+            channel.QueueDeclare(queue: _loggingQueue, durable: false, exclusive: false, autoDelete: false, arguments: null);
             return channel;
         }
 
-        public static void Send(string queue, string payload)
+        public static void Send(string payload)
         {
             var body = Encoding.UTF8.GetBytes(payload);
-            _channel.BasicPublish(exchange: "", routingKey: queue, basicProperties: null, body: body);
+            _channel.BasicPublish(exchange: "", routingKey: _loggingQueue, basicProperties: null, body: body);
         }
     }
 }
