aac801565c4958af841d69ae440a63a4bc2a8a38
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2021-05-01 01:28:46 +0300

    added custome exception base as a Infra

---
+++ CustomeExceptions/InternalBaseException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CustomeExceptions
+{
+    public class InternalBaseException : Exception
+    {
+        public int Code { get; set; }
+        public InternalBaseException(int code, string message) : base(message) 
+        {
+            Code = code;
+        }
+    }
+}
