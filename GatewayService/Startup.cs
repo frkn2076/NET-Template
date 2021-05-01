0bed391472b8e602f91223a7e2bb135984d6dcff
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2021-05-02 00:46:53 +0300

    bidirectional auth added to gateway

--- GatewayService/Startup.cs
+++ GatewayService/Startup.cs
@@ -1,13 +1,17 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Middleware;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using System;
+using System.Text;
 
 namespace GatewayService
 {
@@ -22,6 +26,25 @@ namespace GatewayService
         {
             services.AddControllers();
             services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo { Title = "GatewayService", Version = "v1" }));
+
+            var jwtSecretKey = Environment.GetEnvironmentVariable("JwtSecretKey");
+            var key = Encoding.ASCII.GetBytes(jwtSecretKey);
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = "MySchemeKey";
+            })
+            .AddJwtBearer("MySchemeKey", x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
 
             services.AddOcelot();
         }
