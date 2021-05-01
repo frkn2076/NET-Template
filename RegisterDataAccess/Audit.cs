940ae6aa5bd8838aace73d6a93a5eb869753d2e2
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2021-05-02 00:49:28 +0300

    register data access added

---
+++ RegisterDataAccess/Audit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RegisterDataAccess
+{
+    public abstract class Audit
+    {
+        public DateTime CreatedAt { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime ModifiedAt { get; set; }
+        public string ModifiedBy { get; set; }
+    }
+}
---
+++ RegisterDataAccess/Entities/Register.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegisterDataAccess.Entities
+{
+    public class Register : Audit
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
