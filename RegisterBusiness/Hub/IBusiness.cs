2a7803c464fbf41991d01e1f4908c3c0d7f00d0b
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2021-05-02 00:49:41 +0300

    register business layer added

---
+++ RegisterBusiness/Hub/IBusiness.cs
@@ -0,0 +1,10 @@
+﻿using RegisterBusiness.Models;
+
+namespace RegisterBusiness.Hub
+{
+    public interface IBusiness
+    {
+        bool Login(RegisterDTO model);
+        bool Register(RegisterDTO model);
+    }
+}
---
+++ RegisterBusiness/Models/RegisterDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace RegisterBusiness.Models
+{
+    public class RegisterDTO
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
