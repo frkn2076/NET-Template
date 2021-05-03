0e594c983a43aad08c1739ea3d58e49787c07e5d
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2021-05-03 17:38:42 +0300

    removed column names

--- Register/Register.DataAccess/Entities/Registration.cs
+++ Register/Register.DataAccess/Entities/Registration.cs
@@ -3,16 +3,13 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Register.DataAccess.Entities
 {
-    [Table("registration")]
     public class Registration : Audit
     {
         [Key]
         public int Id { get; set; }
         [Required]
-        [Column("name")]
         public string Name { get; set; }
         [Required]
-        [Column("name")]
         public string Password { get; set; }
     }
 }
