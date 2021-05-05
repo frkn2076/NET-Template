fd368a9bcbb4523db18bff67f6d4243bf5fbfa8f
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2021-05-06 02:25:37 +0300

    removed

--- Register/Register.Service/init.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE "Registrations" (
-	"Id" serial PRIMARY KEY,
-	"Name" VARCHAR ( 50 ) UNIQUE NOT NULL,
-	"Password" VARCHAR ( 50 ) NOT NULL,
-	"CreatedAt" TIMESTAMP,
-	"CreatedBy" VARCHAR ( 50 ),
-	"ModifiedAt" TIMESTAMP,
-	"ModifiedBy" VARCHAR ( 50 )
-);
