a84bad468e13f94dd65914b41da3dda6e9376a44
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2021-05-05 19:23:05 +0300

    mongo db configs and code style changed

--- LogQueue/LogQueue.DataAccess/MongoRepo.cs
+++ LogQueue/LogQueue.DataAccess/MongoRepo.cs
@@ -6,20 +6,23 @@ namespace LogQueue.DataAccess
 {
     public class MongoRepo
     {
-        private static readonly IMongoClient _client = GetMongoClient();
-        private static readonly string _logDatabaseName = Environment.GetEnvironmentVariable("MongoLogDB");
-        private static readonly IMongoDatabase _logDatabase = _client.GetDatabase(_logDatabaseName);
-        private static IMongoClient GetMongoClient()
+        private static readonly IMongoClient _client;
+        private static readonly IMongoDatabase _logDatabase;
+        private static readonly IMongoCollection<BsonDocument> _logCollection;
+        static MongoRepo()
         {
             var user = Environment.GetEnvironmentVariable("MongoLogUser");
             var password = Environment.GetEnvironmentVariable("MongoLogPassword");
             var host = Environment.GetEnvironmentVariable("MongoLogHost");
             var port = Environment.GetEnvironmentVariable("MongoLogPort");
-            var client = new MongoClient($"mongodb://{user}:{password}@{host}:{port}");
-            return client;
-        }
-        private static IMongoCollection<BsonDocument> _collection = _logDatabase.GetCollection<BsonDocument>("Logs");
+            _client = new MongoClient($"mongodb://{user}:{password}@{host}:{port}");
 
-        public static void InsertLog(string log) => _collection.InsertOne(BsonDocument.Parse(log));
+            var logDatabaseName = Environment.GetEnvironmentVariable("MongoLogDB");
+            _logDatabase = _client.GetDatabase(logDatabaseName);
+
+            _logCollection = _logDatabase.GetCollection<BsonDocument>("Logs");
+        }
+
+        public static void InsertLog(string log) => _logCollection.InsertOne(BsonDocument.Parse(log));
     }
 }
