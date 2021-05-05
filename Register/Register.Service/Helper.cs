7c822076dc03b68ceb78b5a6e370dee99be31a45
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2021-05-06 02:26:07 +0300

    created pgsql connections string method explicitly

---
+++ Register/Register.Service/Helper.cs
@@ -0,0 +1,22 @@
+﻿using Register.DataAccess;
+using System;
+
+namespace Register.Service
+{
+    public class Helper
+    {
+        public static string GetPGSQLConnectionString()
+        {
+            var host = Environment.GetEnvironmentVariable("PostgreRegisterHost");
+            var port = Environment.GetEnvironmentVariable("PostgreRegisterPort");
+            var database = Environment.GetEnvironmentVariable("PostgreRegisterDB");
+            var user = Environment.GetEnvironmentVariable("PostgreRegisterUser");
+            var password = Environment.GetEnvironmentVariable("PostgreRegisterPassword");
+            var timeout = Environment.GetEnvironmentVariable("PostgreRegisterTimeout");
+            var pgsqlConnecton = $"Server={host};Port={port};Userid={user};Password={password};Timeout={timeout};Database={database}";
+            return pgsqlConnecton;
+        }
+
+        
+    }
+}
