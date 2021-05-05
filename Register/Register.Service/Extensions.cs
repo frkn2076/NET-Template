240a4efecbacd862bbb27b1eddf5aef46afc4fd3
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2021-05-06 02:25:23 +0300

    run time generic migrator added as extension method

---
+++ Register/Register.Service/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.DependencyInjection;
+using Register.DataAccess;
+
+namespace Register.Service
+{
+    public static class Extensions
+    {
+        public static void MigrateDatabaseAndTables(this IApplicationBuilder app)
+        {
+            using var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>()?.CreateScope();
+            var context = serviceScope?.ServiceProvider.GetRequiredService<AppDBContext>();
+            context.Database.Migrate();
+            RelationalDatabaseCreator databaseCreator = (RelationalDatabaseCreator)context.Database.GetService<IDatabaseCreator>();
+            databaseCreator.CreateTables();
+        }
+    }
+}
