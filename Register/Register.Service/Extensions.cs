e4cca33ba8aa1dc7f5c3fb552d28c418f6332077
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2021-05-08 02:39:21 +0300

    code style edited

--- Register/Register.Service/Extensions.cs
+++ Register/Register.Service/Extensions.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.Extensions.DependencyInjection;
 using Register.DataAccess;
+using System;
 
 namespace Register.Service
 {
@@ -11,11 +12,18 @@ namespace Register.Service
     {
         public static void MigrateDatabaseAndTables(this IApplicationBuilder app)
         {
-            using var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>()?.CreateScope();
-            var context = serviceScope?.ServiceProvider.GetRequiredService<AppDBContext>();
-            context.Database.Migrate();
-            RelationalDatabaseCreator databaseCreator = (RelationalDatabaseCreator)context.Database.GetService<IDatabaseCreator>();
-            databaseCreator.CreateTables();
+            try
+            {
+                using var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>()?.CreateScope();
+                var context = serviceScope?.ServiceProvider.GetRequiredService<AppDBContext>();
+                context.Database.Migrate();
+                RelationalDatabaseCreator databaseCreator = (RelationalDatabaseCreator)context.Database.GetService<IDatabaseCreator>();
+                databaseCreator.CreateTables();
+            }
+            catch (Exception ex)
+            {
+                //log
+            }
         }
     }
 }
