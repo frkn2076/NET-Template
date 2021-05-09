c19ebde92d5ca42c003f099fec3bf20ece2baf00
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2021-05-10 02:55:20 +0300

    Authentication entity created

---
+++ Register/Register.DataAccess/Entities/Authentication.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Register.DataAccess.Entities
+{
+    public class Authentication : Audit
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string AccessToken { get; set; }
+        [Required]
+        public string RefreshToken { get; set; }
+    }
+}
