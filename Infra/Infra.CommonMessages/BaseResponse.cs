469a5824752e85d658322c8180a1fca6d15ac452
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2021-05-10 02:54:41 +0300

    token added to baseresponse structure

--- Infra/Infra.CommonMessages/BaseResponse.cs
+++ Infra/Infra.CommonMessages/BaseResponse.cs
@@ -14,5 +14,9 @@
 
         public bool IsError { get; set; }
         public string Message { get; set; }
+        public string AccessToken { get; set; }
+        public int AccessTokenExpiresIn { get; set; }
+        public string RefreshToken { get; set; }
+        public string RefreshTokenExpiresIn { get; set; }
     }
 }
