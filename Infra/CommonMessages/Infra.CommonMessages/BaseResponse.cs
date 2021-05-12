a0372911429e92dc228e23fa18657819506629ce
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2021-05-13 00:53:25 +0300

    BaseResponse added to Infra

---
+++ Infra/CommonMessages/Infra.CommonMessages/BaseResponse.cs
@@ -0,0 +1,24 @@
+﻿namespace Infra.CommonMessages
+{
+    public class BaseResponse
+    {
+        public static BaseResponse Success => new(string.Empty);
+        public static BaseResponse Fail => new("Something went wrong");
+        public static BaseResponse Create(string message) => new(message);
+
+        private BaseResponse(string message)
+        {
+            IsError = !string.IsNullOrWhiteSpace(message);
+            Message = message;
+        }
+
+        public BaseResponse() { }
+
+        public bool IsError { get; set; }
+        public string Message { get; set; }
+        public string AccessToken { get; set; }
+        public int AccessTokenExpiresIn { get; set; }
+        public string RefreshToken { get; set; }
+        public int RefreshTokenExpiresIn { get; set; }
+    }
+}
