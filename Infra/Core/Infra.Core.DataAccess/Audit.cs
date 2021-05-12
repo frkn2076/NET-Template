b83286ab511ab394af35d8e698b89b6d61b7ef13
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2021-05-13 00:53:42 +0300

    Audit added to Infra

---
+++ Infra/Core/Infra.Core.DataAccess/Audit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Infra.Core.DataAccess
+{
+    public abstract class Audit
+    {
+        public DateTime CreatedAt { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime ModifiedAt { get; set; }
+        public string ModifiedBy { get; set; }
+    }
+}
