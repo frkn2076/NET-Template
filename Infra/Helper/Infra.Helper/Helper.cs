acaefa97d2e0c178f13f2896680e7ac8d1785aa1
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2021-05-13 00:54:11 +0300

    Helper added to Infra

---
+++ Infra/Helper/Infra.Helper/Helper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Infra.Helper
+{
+    public class Helper
+    {
+        public static string GetPostgreDatabaseConnection(string database)
+        {
+            var host = Environment.GetEnvironmentVariable("PostgreRegisterHost");
+            var port = Environment.GetEnvironmentVariable("PostgreRegisterPort");
+            var user = Environment.GetEnvironmentVariable("PostgreRegisterUser");
+            var password = Environment.GetEnvironmentVariable("PostgreRegisterPassword");
+            var timeout = Environment.GetEnvironmentVariable("PostgreRegisterTimeout");
+            var pgsqlConnecton = $"Server={host};Port={port};Userid={user};Password={password};Timeout={timeout};Database={database}";
+            return pgsqlConnecton;
+        }
+    }
+}
