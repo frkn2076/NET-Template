2bb3c084000ecab59bf92dfe71e1c427bb1f9a8c
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2021-05-13 00:54:55 +0300

    Localization respository implemented

---
+++ Infra/Localization/Infra.Localizer.Repository/ILocalizationRepo.cs
@@ -0,0 +1,9 @@
+﻿namespace Infra.Resource.Repository
+{
+    public interface ILocalizationRepo
+    {
+        public string First(string language, string key);
+        public void InsertOrUpdate(string language, string key, string value);
+        public int SaveChanges();
+    }
+}
---
+++ Infra/Localization/Infra.Localizer.Repository/Implementation/LocalizationRepo.cs
@@ -0,0 +1,32 @@
+﻿using Infra.Resource.DataAccess;
+using Infra.Resource.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace Infra.Resource.Repository.Implementation
+{
+    public class LocalizationRepo : ILocalizationRepo
+    {
+        private readonly LocalizerDBContext _context;
+
+        public LocalizationRepo(LocalizerDBContext context) => _context = context;
+
+        private string First(string language, string key) =>
+            _context.Localizations.AsNoTracking().FirstOrDefault(x => x.Language == language && x.Key == key).Value;
+
+        private void InsertOrUpdate(string language, string key, string value)
+        {
+            var existLocalization = _context.Localizations.FirstOrDefault(x => x.Language == language && x.Key == key);
+            if(existLocalization == null)
+                _context.Localizations.Add(new Localization() { Language = language, Key = key, Value = value });
+            else
+                existLocalization.Value = value;
+        }
+
+        private int SaveChanges() => _context.SaveChanges();
+
+        string ILocalizationRepo.First(string language, string key) => First(language, key);
+        void ILocalizationRepo.InsertOrUpdate(string language, string key, string value) => InsertOrUpdate(language, key, value);
+        int ILocalizationRepo.SaveChanges() => SaveChanges();
+    }
+}
